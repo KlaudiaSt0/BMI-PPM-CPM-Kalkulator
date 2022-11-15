2409ae94e552872315c78ecb9dd1a65a08903ee7
KlaudiaSt0 <klaudia.m.stepniak@gmail.com>
2022-11-15 12:56:46 +0100

    change of API

--- src/pages/CPM/getInspired.js
+++ src/pages/CPM/getInspired.js
@@ -2,27 +2,19 @@ import React, {useState, useEffect} from "react";
 
 const GetInspired = ({insp}) => {
     const [data, setData] = useState([]);
-        const api = "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
+        const api = "https://api.edamam.com/api/recipes/v2?type=any&app_id=ba52b8b0&app_key=deca89dac3bc432bc19f451f51517084&diet=balanced&health=Mediterranean&calories=0-380&imageSize=SMALL&random=true";
         useEffect(() => {
         fetch(`${api}`)
             .then(res => res.json())
-            .then(json => setData(json.meals))
+            .then(json => setData(json))
             .catch(err => console.error(err))
         }, [insp]);
 
-    const tenRandom = (data) => {
-        const tenRandom = [];
-        for(let i = 1; i <=10; i++) {
-            let randomNumber = Math.floor(Math.random() * data.length + 1);
-            tenRandom.push(data[randomNumber])
-        }
-        console.log(tenRandom)
-        return tenRandom;
-    }
-
+        const recipesArray = [...data.hits];
+console.log(recipesArray)
 if(insp === true) {
-    return <div><h3>Urozmaicona dieta to podstawa zdrowia! Kiedy ostatnio w Twojej diecie znalazły się:</h3>
-        <ul>{tenRandom(data).map(el => <li key={el.idIngredient}>{el.strIngredient}</li>)}</ul></div>
+    return <div><h3 className="header_3">Urozmaicona dieta to podstawa zdrowia! Kiedy ostatnio w Twojej diecie znalazły się:</h3>
+        <ul>{recipesArray.map(el => <li> <b>{el.recipe.label}</b> - meal type: {el.recipe.mealType}, kcal: {(el.recipe.calories).toFixed(2)} kcal</li>)}</ul></div>
 } else {
     return null;
 }
